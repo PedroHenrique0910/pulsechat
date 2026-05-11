a29d804b6fe4a7bb56cce5cf9b165b23acd02d5a
Pedro Henrique <contatopedrohq@gmail.com>
2026-05-11 20:17:45 -0300

    feat(auth): add initial private route structure

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
 import express from "express";
 import cors from "cors";
 
-
 const app = express();
 
 app.use(cors())
-app.use(express.json()) // <- importante pra ler req.body
+app.use(express.json()) 
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import PrivateRoute from './src/Components/PrivateRoute.jsx'
 import Home from './src/pages/Home/Home.jsx'
 import Chat from './src/pages/Chat/Chat.jsx'
 import Login from './src/pages/Login/Login.jsx'
@@ -7,8 +8,10 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/chat" element={<Chat />} />
+      <Route element={<PrivateRoute />}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/chat" element={<Chat />} />
+      </Route>
     </Routes>
   )
 }
