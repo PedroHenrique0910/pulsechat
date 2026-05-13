76f13a176d537b218fe9aded996d8d3d8bb4f301
Pedro Henrique <contatopedrohq@gmail.com>
2026-05-13 20:22:11 -0300

    chore: setup mongodb with docker and connect to backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from "express";
 import cors from "cors";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
 app.use(cors())
 app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB conectado!"))
+    .catch((err) => console.log("Erro ao conectar:", err))
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
